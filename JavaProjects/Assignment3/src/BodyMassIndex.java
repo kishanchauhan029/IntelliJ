b19c55e4986b80e45f7d4c49b98551d9bb4248ad
kishanchauhan029 <kishanchauhan029@gmail.com>
2022-09-20 19:41:23 -0400

    printingFeatureAdded

---
+++ JavaProjects/Assignment3/src/BodyMassIndex.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class BodyMassIndex {
+    public static void main(String[] args) {
+        System.out.println(getHeightAndWeight());
+    }
+
+    public static float getHeightAndWeight(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Your Height:");
+        float weight = (float) scanner.nextDouble();
+        System.out.println("Your Weight:");
+        float height = scanner.nextFloat();
+        System.out.println("Your BMI is:" + weight/(height*height));
+        return weight/(height*height);
+    }
+}
